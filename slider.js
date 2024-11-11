6a28d12f930b582b6f5ae017210f8a78ac63323e
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2024-11-11 16:49:04 -0300

    Melhora do sistema de slide e evito de tradução!

--- slider.js
+++ slider.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1.5,
   spaceBetween: 20,
   loop: true,
+  autoplay: {
+    delay: 8000,
+    pauseOnMouseEnter: true,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
